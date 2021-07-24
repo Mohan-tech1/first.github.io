fa40ca9c2708e0ed3ca9686703cb7bb0b75e3c08
Mohan-tech1 <mohan202145@gmail.com>
2021-07-25 02:04:02 +0530

    email message

--- index.js
+++ index.js
@@ -18,3 +18,40 @@
       }, false)
     })
 })();
+//for contact
+
+function messagecontact() {
+  var email = document.getElementById('email').value;//contact email Id
+  var comment = document.getElementById('comment').value;//contact comment
+  var name = document.getElementById('name').value;//contact name
+  //contact body
+  // alert(email)
+  Email.send({
+    Host: 'smtp.gmail.com',
+    Username: "mtech737703@gmail.com",
+    Password: "9058843284",
+    To: email,
+    From: "mtech737703@gmail.com",
+    Subject: "MTech Foundation",
+    Body: `Hi ${name} \n what can I help you. \n ohh sorry \n you are contact my orgniztion for this metter ${comment} .but I'm really sorry I can't help you because this site is fronted so I can't replly any Querry.\n And after all thanks for contact me and subcribe my website.\n\n with regards\n Mohan Lal Sharma `,
+  }).then(function (message) {
+    alert("thanks for contact")
+  });
+}
+
+// for subscription
+function sendMail() {
+  var email = document.getElementById('subc_email').value;//contact comment
+  Email.send({
+    Host: 'smtp.gmail.com',
+    Username: "mtech737703@gmail.com",
+    Password: "9058843284",
+    To: email,
+    From: "mtech737703@gmail.com",
+    Subject: "MTech Foundation",
+    Body: `thanks for subcribe`,
+  }).then(function (message) {
+    alert(`thanks for subcribe`)
+  });
+  // location.reload();
+}
